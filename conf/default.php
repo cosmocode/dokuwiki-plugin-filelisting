04ce195aba109526681fa3d69fb1b8f0568a50f2
Szymon Olewniczak <solewniczak@rid.pl>
2017-10-13 09:06:13 +0200

    initial wizard generated structure

---
+++ conf/default.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Default settings for the filelisting plugin
+ *
+ * @author Szymon Olewniczak <dokuwiki@cosmocode.de>
+ */
+
+//$conf['fixme']    = 'FIXME';
---
+++ conf/metadata.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Options for the filelisting plugin
+ *
+ * @author Szymon Olewniczak <dokuwiki@cosmocode.de>
+ */
+
+
+//$meta['fixme'] = array('string');
+
