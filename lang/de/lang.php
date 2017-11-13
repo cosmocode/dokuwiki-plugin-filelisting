a1cbd1a42519eca08f15363891ea6d2893a99582
Michael Große <grosse@cosmocode.de>
2017-11-13 14:25:42 +0100

    L10n: Add German language files

---
+++ lang/de/lang.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * German language file for filelisting plugin
+ *
+ * @author Szymon Olewniczak <dokuwiki@cosmocode.de>
+ */
+
+$lang['header filename'] = 'Name';
+$lang['header filesize'] = 'Größe';
+$lang['header filedate'] = 'Datum';
+
+$lang['files_in_namespace'] = 'Dateien im Namensraum <span>%s</span>';
+
+$lang['js']['filter_label'] = 'Filter';
+
+//Setup VIM: ex: et ts=4 :
---
+++ lang/de/settings.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * english language file for filelisting plugin
+ *
+ * @author Szymon Olewniczak <dokuwiki@cosmocode.de>
+ */
+
+$lang['defaulttoggle'] = 'Zeige Dateiauflistung standardmäßig an';
+
+
+
+//Setup VIM: ex: et ts=4 :
