90c93bd6894ea239d0de9cb524509e9842690dae
Michael Große <grosse@cosmocode.de>
2017-11-15 17:23:40 +0100

    fix: files in a baseNS except :, couldn't be searched

--- script.js
+++ script.js
@@ -202,8 +202,8 @@
                 }
             };
 
-        //files in root namespace are always visible
-        $files.filter('[data-childOf=""]').each(filterCallback);
+        //files in base namespace are always visible
+        $files.filter('[data-childOf="' + this.options.baseNamespace + '"]').each(filterCallback);
 
         //get namespaces
         $dirs.filter(function() {
