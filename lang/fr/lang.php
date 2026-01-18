234609655e350150b1225e6918e92c6b6c8a9e74
Fabrice DEJAIGHER <fabrice@chtiland.com>
2026-01-18 12:55:41 +0100

    Translation update (fr)

---
+++ lang/fr/lang.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Fabrice DEJAIGHER <fabrice@chtiland.com>
+ */
+$lang['header filename']       = 'Nom';
+$lang['header filesize']       = 'Taille';
+$lang['header filedate']       = 'Date';
+$lang['files_in_namespace']    = 'Fichiers dans la catégorie <span>%s</span>';
+$lang['delete_selected']       = 'Supprimer la sélection';
+$lang['js']['filter_label']    = 'Filtre';
+$lang['js']['delete_confirm']  = 'Êtes-vous certain de vouloir supprimer ce(s) fichier(s) ?';
---
+++ lang/fr/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Fabrice DEJAIGHER <fabrice@chtiland.com>
+ */
+$lang['defaulttoggle']         = 'Listes dépliées par défaut.';
+$lang['remember_state_per_page'] = 'Méthode de mémorisation l\'état de la liste';
+$lang['remember_state_per_page_o_page'] = 'Par page (surtout si vous utilisez l\'extension comme en syntaxe)';
+$lang['remember_state_per_page_o_wiki'] = 'Globale (surtout quand vous utilisez cette extension comme partie du modèle wiki)';
